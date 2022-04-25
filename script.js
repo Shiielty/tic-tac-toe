7fb52be9426123674e8d133d1183f4e407841430
Shiielty <kazutonine@gmail.com>
2022-04-25 23:47:01 +0700

    feat(*): create gameBoard module (testing)

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-const btn = document.querySelector("button");
 
-btn.addEventListener("click", () => alert("Hi! Let's start this project!"))
+
+const gameBoard = (() => {
+    
+    const btn = document.querySelector("button");
+
+    const alertBtn = () => {
+        btn.addEventListener("click", () => alert("Hi! Let's start this project!"))
+    };
+
+    return {alertBtn}
+
+    // let board = [];
+
+    // const div = document.createElement("div");
+
+    // // create 9 box and insert to the board array
+    // for (let i = 0; i < 6; i++) {
+        
+    
+})();
