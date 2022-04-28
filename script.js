e676085be8e48fee8098e5d4b6728f649e68985a
Shiielty <kazutonine@gmail.com>
2022-04-28 23:07:36 +0700

    feat(*): render the tic tac toe board in JS instead in the HTML

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
-
-
 const gameBoard = (() => {
-    
+
+    const board = [];
+
+    //selector
     const btn = document.querySelector("button");
+    const gameBoard = document.querySelector(".game-board");
+
+    //create tiles
+    const makeTiles = () => {
+        for (let i = 0; i < 9; i++) {
+            let tile = document.createElement("div");
+            tile.classList.add("tile");
+            tile.dataset.index = i;
+            board.push(tile);
+            console.log(tile);
+            tile = null;
+            console.log(tile);
+        }
+    }
+
+    //render tiles
+    const render = () => {
+        board.forEach((tile) => {
+            gameBoard.appendChild(tile);
+        });
+    }
+
+    const init = () => {
+        makeTiles();
+        render();
+    }
 
     const alertBtn = () => {
         btn.addEventListener("click", () => alert("Hi! Let's start this project!"))
     };
 
-    return {alertBtn}
-
-    // let board = [];
-
-    // const div = document.createElement("div");
-
-    // // create 9 box and insert to the board array
-    // for (let i = 0; i < 6; i++) {
+    return {init, alertBtn}
         
     
 })();
+
+gameBoard.init();
+
+
+// testing does the dataset are correctly used :: problem is I can't put this variable and function in the gameBoard yet
+const tiles = document.querySelectorAll(".tile");
+tiles.forEach((tile) => {
+    tile.addEventListener("click", () => console.log(tile.dataset.index));
+});
