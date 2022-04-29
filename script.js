d271fb3063d7eb302da6bdaf8ed0917f0b7e170c
Shiielty <kazutonine@gmail.com>
2022-04-29 20:28:47 +0700

    feat(*): add gameController module :: move events function in gameBoard module to gameController module

--- script.js
+++ script.js
@@ -1,31 +1,52 @@
 const gameBoard = (() => {
 
     const board = [];
-
-    //selector
-    const btn = document.querySelector("button");
-    const gameBoard = document.querySelector(".game-board");
     
-
     //create tiles
     const makeTiles = () => {
         for (let i = 0; i < 9; i++) {
-            let tile = document.createElement("div");
+            const tile = document.createElement("div");
             tile.classList.add("tile");
             tile.dataset.index = i;
+            
+            const X = document.createElement("span");
+            X.classList.add("x");
+            X.dataset.index = i;
+            X.textContent = "X";
+            
+            const O = document.createElement("span");
+            O.classList.add("o");
+            O.dataset.index = i;
+            O.textContent = "O";
+            
+            tile.appendChild(X);
+            tile.appendChild(O);
             board.push(tile);
         }
     }
 
     //render tiles
     const render = () => {
+        const gameBoard = document.querySelector(".game-board");
+        
         board.forEach((tile) => {
             gameBoard.appendChild(tile);
         });
     }
 
+    // initiate the gameBoard object's functions
+    const init = () => {
+        makeTiles();
+        render();
+    }
+    
+    return {init}  
+})();
+
+const gameController = (() => {
+
     const consoleBtn = () => {
-        console.log("Hello world!")
+        console.log("Hello world!");
     }
 
     const checkIndex = (e) => {
@@ -33,24 +54,22 @@ const gameBoard = (() => {
     }
 
     const events = () => {
-        btn.addEventListener("click", consoleBtn)
-        const tiles = document.querySelectorAll(".tile");
-        tiles.forEach((tile) => {
+        const btn = document.querySelector("button");
+        const test = document.querySelectorAll(".tile");
+
+        btn.addEventListener("click", consoleBtn);
+        test.forEach((tile) => {
             checkIndex(tile);
         });
     };
 
-    // initiate the gameBoard object's functions
     const init = () => {
-        makeTiles();
-        render();
         events();
     }
 
-    return {init}
-        
-    
+    return{init};
 })();
 
-gameBoard.init();
 
+gameBoard.init();
+gameController.init();
